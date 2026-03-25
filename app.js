28553807c7397fa0ca0fdc4ce9434c5d213c6ad3
Amolpatil113 <amolmpatil08@gmail.com>
2026-03-25 14:27:25 +0530

    routes added separately

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const Campground = require('./models/campground');
 const app = express();
 const methodOverride = require('method-override');
 const Review = require ('./models/review')
-
+const campgrounds=require('./routes/campgrounds')
 
 
 app.use(express.static(path.join(__dirname, 'public')));
@@ -49,43 +49,11 @@ const validateReview = (req,res,next)=>{
         next();
     }
 }
+app.use('/campgrounds', campgrounds)
 app.get('/',(req,res)=>{
     res.render('home')
 })
-app.get('/campgrounds',catchAsync(async(req,res)=>{
-   const campgrounds=await Campground.find({});
-    res.render('campgrounds/index', { campgrounds })
-}))
-app.get('/campgrounds/new',catchAsync((req,res)=>{
-   res.render('campgrounds/new') 
-}))
 
-app.post('/campgrounds',
-validateCampground,
-catchAsync(async(req,res)=>{
-    const campground = new Campground(req.body.campground);
-    await campground.save();
-    res.redirect(`/campgrounds/${campground._id}`);
-}));
-app.get('/campgrounds/:id',catchAsync(async(req,res,next)=>
-{
-    const campground=await Campground.findById(req.params.id).populate('reviews');
-    res.render('campgrounds/show',{campground});
-}));
-app.get('/campgrounds/:id/edit',catchAsync(async(req,res)=>{
-    const campground=await Campground.findById(req.params.id)
-    res.render('campgrounds/edit',{campground});
-}))
-app.put('/campgrounds/:id',validateCampground,catchAsync(async(req,res)=>{
-    const {id}=req.params;
-    const campground=await Campground.findByIdAndUpdate(id,{...req.body.campground})
-    res.redirect(`/campgrounds/${campground._id}`)
-}))
-app.delete('/campgrounds/:id',catchAsync(async (req,res)=>{
-    const {id}=req.params;
-    await Campground.findByIdAndDelete(id);
-    res.redirect('/campgrounds');
-}));
 app.post('/campgrounds/:id/reviews',validateReview, catchAsync(async (req,res)=>{
         const campground = await Campground.findById(req.params.id);
         const review =new Review (req.body.review);
