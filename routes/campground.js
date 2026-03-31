a158ea587ced9d275ad7490bef3263e7085af09d
Amolpatil113 <amolmpatil08@gmail.com>
2026-03-31 15:24:26 +0530

    Add review routes with validation and error handling

--- routes/campground.js
+++ routes/campground.js
@@ -1,7 +1,21 @@
 const express = require('express');
 const router = express.Router(); 
+const catchAsync=require('../utils/catchAsync');
+const ExpressError=require('../utils/ExpressError');
+const Campground = require('../models/campground');
+const Joi =require('joi')
+const {campgroundSchema}=require('../schemas.js')
 
 
+const validateCampground=(req,res,next)=>{
+    const {error}=campgroundSchema.validate(req.body);
+    if(error){
+        const msg=error.details.map(el=>el.message).join(',')
+        throw new ExpressError(msg,400)
+    }else{
+        next();
+    }
+}
 
  router.get('/',catchAsync(async(req,res)=>{
    const campgrounds=await Campground.find({});
@@ -11,8 +25,7 @@ router.get('/new',catchAsync((req,res)=>{
    res.render('campgrounds/new') 
 }))
 
-router.post('/',validateCampground,
-catchAsync(async(req,res)=>{
+router.post('/', validateCampground ,catchAsync(async(req,res)=>{
     const campground = new Campground(req.body.campground);
     await campground.save();
     res.redirect(`/campgrounds/${campground._id}`);
